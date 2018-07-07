e034c90c699c2cec393c683074cc0637a58c8c87
cn-src <public@javaer.cn>
2018-07-08 01:51:01 +0800

    init

---
+++ test/test_fix_q.py
@@ -0,0 +1,14 @@
+# -* - coding: UTF-8 -* -
+from unittest import TestCase
+
+from main import fix_q
+
+
+class TestFix_q(TestCase):
+    def test_fix_q(self):
+        self.assertEquals(fix_q('a:demo'), 'a:"demo"')
+        self.assertEquals(fix_q('a:"demo"'), 'a:"demo"')
+        self.assertEquals(fix_q('g:demo'), 'g:"demo"')
+        self.assertEquals(fix_q('g:"demo"'), 'g:"demo"')
+        #  不正确的写法，保持原样
+        self.assertEquals(fix_q('a:"demo'), 'a:"demo')
